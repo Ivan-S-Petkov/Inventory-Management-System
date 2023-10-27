5f6174da8652368ec081facb705e33330ad5ffbc
Ivan Petkov <ivan.slv.petkov@gmail.com>
2023-10-27 15:30:53 +0300

    added ItemService class

--- scr/com/inventory/app/services/Item.java
+++ scr/com/inventory/app/services/Item.java
@@ -1,7 +1,9 @@
-package com.inventory.app;
+package com.inventory.app.models;
 
 public interface Item {
+    public String toString();
     public void getItemDetails();
     public double calculateValue();
     public void showItemDescription();
+
 }
---
+++ scr/com/inventory/app/services/ItemService.java
@@ -0,0 +1,17 @@
+package com.inventory.app.services;
+
+import com.inventory.app.models.InventoryItem;
+
+public class ItemService implements Item {
+
+    @Override
+    public void getDetails(InventoryItem item) {
+        System.out.printf("%s - %s : %.2f %n", item.getName(), item.getDescription(), item.getQuantity());
+    }
+
+    @Override
+    public String showDescription(InventoryItem item) {
+        return item.getDescription();
+    }
+
+}
