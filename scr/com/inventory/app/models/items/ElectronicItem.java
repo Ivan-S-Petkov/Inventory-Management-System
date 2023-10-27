a8cd9d1a69dd66daa89a57db9f4e402e2c7271c0
Ivan Petkov <ivan.slv.petkov@gmail.com>
2023-10-27 16:59:59 +0300

    implemented Sellable interface in ElectornicItem class

--- scr/com/inventory/app/models/items/ElectronicItem.java
+++ scr/com/inventory/app/models/items/ElectronicItem.java
@@ -1,11 +1,12 @@
-package com.inventory.app.models.itemTypes;
+package com.inventory.app.models.items;
 
 import com.inventory.app.models.InventoryItem;
 
 public class ElectronicItem extends InventoryItem implements Breakable, Sellable {
 
-    public ElectronicItem(int id, double quantity, String name, String description, String type) {
-        super(id, quantity, name, description, type);
+
+    public ElectronicItem(int id, String name, String description, double quantity, String type, String category, double price) {
+        super(name, description, id, quantity, type, category, price);
     }
 
     @Override
@@ -19,12 +20,17 @@ public class ElectronicItem extends InventoryItem implements Breakable, Sellable
     }
 
     @Override
-    public void setPrice() {
-
+    public void setPrice(double price) {
+        this.price = price;
     }
 
     @Override
-    public void getPrice() {
+    public double getPrice() {
+        return price;
+    }
 
+    @Override
+    public double calculateValue() {
+        return price * quantity;
     }
 }
