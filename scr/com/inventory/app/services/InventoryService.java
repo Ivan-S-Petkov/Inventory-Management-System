280c02f19511a5b6b0d362c9ebcd8f898d64384f
Ivan Petkov <ivan.slv.petkov@gmail.com>
2023-10-28 09:15:13 +0300

    added category and price variable in InventoryService, inventory list extended to accept AssetItem class

--- scr/com/inventory/app/services/InventoryService.java
+++ scr/com/inventory/app/services/InventoryService.java
@@ -1,31 +1,36 @@
 package com.inventory.app.services;
 
 import com.inventory.app.models.Inventory;
+import com.inventory.app.models.items.AssetItem;
 import com.inventory.app.models.items.ElectronicItem;
 import com.inventory.app.models.items.FragileItem;
 import com.inventory.app.models.items.GroceryItem;
 
 import java.util.Objects;
 
-public class InventoryService {
+public class InventoryService extends Inventory{
 
-    public static boolean addInventory(Inventory inventory, String name, String description, double quantity, String type) {
+    public static boolean addInventory(Inventory inventory, String name, String description, double quantity, String type, String category, double price) {
         int ID = inventory.getInventory().size();
         boolean flag = false;
         if (Objects.equals(type, "electronic")) {
-            inventory.getInventory().add(new ElectronicItem(ID, quantity, name, description, type));
+            inventory.getInventory().add(new ElectronicItem(ID, name, description, quantity, type, category, price));
             flag = true;
         } else if (Objects.equals(type, "fragile")) {
-            inventory.getInventory().add(new FragileItem(ID, quantity, name, description, type));
+            inventory.getInventory().add(new FragileItem(ID, name, description,quantity, type, category, price));
             flag = true;
         } else if (Objects.equals(type, "grocery")) {
-            inventory.getInventory().add(new GroceryItem(ID, quantity, name, description, type));
+            inventory.getInventory().add(new GroceryItem(ID, name, description,quantity, type, category, price));
+            flag = true;
+        } else if (Objects.equals(type, "asset")) {
+            inventory.getInventory().add(new AssetItem(ID, name, description,quantity, type, category, price));
             flag = true;
         }
         return flag;
     }
 
     public static void listInventoryItems(Inventory inventory) {
-        inventory.getInventory().forEach(inventoryItem -> System.out.printf("%s - %s: %.2f %n", inventoryItem.getName(), inventoryItem.getDescription(), inventoryItem.getQuantity()));
+        ItemService itemService = new ItemService();
+        inventory.getInventory().forEach(itemService::getDetails);
     }
 }
