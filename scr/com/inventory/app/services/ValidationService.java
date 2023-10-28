d8d7f496c8dea9b7704db9884e66bbd2e7bc6c58
Ivan Petkov <ivan.slv.petkov@gmail.com>
2023-10-28 09:18:55 +0300

    additional validation checks added to the Validation Service methods

--- scr/com/inventory/app/services/ValidationService.java
+++ scr/com/inventory/app/services/ValidationService.java
@@ -6,41 +6,58 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class Validation {
+public class ValidationService {
 
-    private final List<String> types = new ArrayList<>(Arrays.stream(new String[]{"electronic", "fragile", "grocery"}).toList());
+    private static final List<String> types = new ArrayList<>(Arrays.stream(new String[]{"electronic", "fragile", "grocery","asset"}).toList());
 
 
-    private boolean checkName(Inventory inventory, String name) {
+    static boolean validateName(Inventory inventory, String name) {
+        if(name.isEmpty()){
+            System.out.println("The name is empty!");
+            System.out.print("Please type the name again: ");
+            return false;
+        }
+        //Check if name already exists
         if (inventory.getInventory().isEmpty() || inventory.getInventory().stream().noneMatch(inventoryItem -> inventoryItem.getName().contains(name))){
             return true;
         } else{
             System.out.println("Name already exist!");
+            System.out.print("Please type the name again: ");
             return false;
         }
     }
 
-    private boolean checkType(String type) {
+
+
+    static boolean validateType(String type) {
         if (types.contains(type)) {
             return true;
         } else {
             System.out.println("Invalid type!");
+            System.out.print("Please specify the type again: ");
             return false;
         }
     }
 
-    private boolean checkQuantity(String quantity) {
+    static boolean validateQuantity(String quantity) {
         try {
-            Double number = Double.parseDouble(quantity);
-            return true;
+            double number = Double.parseDouble(quantity);
+            if (number>0){
+            return true;}
+            else{
+                System.out.println("Quantity must be positive number!");
+                System.out.print("Please type the quantity again: ");
+                return false;
+            }
         } catch (NumberFormatException e) {
             System.out.println("Quantity is not a number!");
+            System.out.print("Please type the quantity again: ");
             return false;
         }
     }
 
-    public boolean validateAddInputs(Inventory inventory, String name, String description, String quantity, String type) {
-        return checkName(inventory, name) && checkQuantity(quantity) && checkType(type);
+    private static boolean checkCategory(String category){
+        return !category.isEmpty();
     }
 
 }
