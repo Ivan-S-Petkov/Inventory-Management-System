4ae31a96710096545ba0a96694a908e853c124c6
Ivan Petkov <ivan.slv.petkov@gmail.com>
2023-11-03 12:37:10 +0200

    fixed add method, modified remove and viewCart methods in CartService

--- scr/com/inventory/app/services/CartService.java
+++ scr/com/inventory/app/services/CartService.java
@@ -12,7 +12,7 @@ public class CartService {
         if (item != null && validateAvailableQuantity(item, quantity)) {
             InventoryItem itemToAdd = new InventoryItem(item);
             itemToAdd.setQuantity(quantity);
-            user.getCart().add(item);
+            user.getCart().add(itemToAdd);
             System.out.println("Added to cart successfully! ");
         } else {
             System.out.println("Not enough quantity");
@@ -23,7 +23,9 @@ public class CartService {
     public static void remove(User user, String product) {
         if (validateCartItemExist(user, product)) {
             InventoryItem itemToRemove = getCartItemByName(user, product);
+            assert itemToRemove != null;
             user.getCart().remove(itemToRemove);
+            System.out.printf("%s removed successfully! %n",itemToRemove.getName().substring(0,1).toUpperCase()+itemToRemove.getName().substring(1));
         }
     }
 
@@ -39,6 +41,9 @@ public class CartService {
     public static void viewCart(User user) {
         for (InventoryItem item : user.getCart()) {
             System.out.printf("%s - %s : %.2f %n", item.getName(), item.getDescription(), item.getQuantity());
+        }
+        if (user.getCart().isEmpty()){
+            System.out.println("Cart is empty!");
         }
     }
 }
